a75097e4ef732ca2bc6319ea06d286394a3e84ca
noword <noword.forever@gmail.com>
2023-11-02 20:53:34 +0800

    update

--- lib3dstex.py
+++ lib3dstex.py
@@ -1,5 +1,6 @@
 from ctypes import *
 import os
+from PIL import Image
 
 __path = os.path.dirname(os.path.realpath(__file__))
 DLL_NAME = 'lib3dstex.dll'
@@ -8,32 +9,44 @@ if not os.path.exists(DLL_FULL_NAME):
     DLL_FULL_NAME = os.path.join(os.getcwd(), DLL_NAME)
 DLL = cdll.LoadLibrary(DLL_FULL_NAME)
 
-FORMATS = {'RGBA8888': 0,
-           'RGB888': 1,
-           'RGBA5551': 2,
-           'RGB565': 3,
-           'RGBA4444': 4,
-           'LA88': 5,
-           'HL8': 6,
-           'L8': 7,
-           'A8': 8,
-           'LA44': 9,
-           'L4': 10,
-           'A4': 11,
-           'ETC1': 12,
-           'ETC1_A4': 13, }
+FORMATS = {'RGBA8888': (0, 'RGBA'),
+           'RGB888': (1, 'RGB'),
+           'RGBA5551': (2, 'RGBA'),
+           'RGB565': (3, 'RGB'),
+           'RGBA4444': (4, 'RGBA'),
+           'LA88': (5, 'LA'),
+           'HL8': (6, 'RGB'),
+           'L8': (7, 'L'),
+           'A8': (8, 'L'),
+           'LA44': (9, 'LA'),
+           'L4': (10, 'L'),
+           'A4': (11, 'L'),
+           'ETC1': (12, 'RGB'),
+           'ETC1_A4': (13, 'RGBA')}
 
 
-def __codec(size_func, codec_func, buf, width, height, fmt):
+def __codec(size_func: CFUNCTYPE, codec_func: CFUNCTYPE, buf: bytes, width: int, height: int, fmt: str) -> bytes:
     size = size_func(width, height, fmt)
     out = create_string_buffer(size)
     codec_func(buf, width, height, fmt, out)
     return out.raw
 
 
-def encode(buf, width, height, fmt):
+def encode(buf: bytes, width: int, height: int, fmt: str) -> bytes:
     return __codec(DLL.get_encode_size, DLL.encode, buf, width, height, fmt)
 
 
-def decode(buf, width, height, fmt):
+def decode(buf: bytes, width: int, height: int, fmt: str) -> bytes:
     return __codec(DLL.get_decode_size, DLL.decode, buf, width, height, fmt)
+
+
+def bin2image(buf: bytes, width: int, height: int, fmt: str) -> Image:
+    im = Image.new(FORMATS[fmt][1], (width, height))
+    im.frombytes(decode(buf, width, height, fmt))
+    return im
+
+
+def image2bin(im: Image, fmt: str) -> bytes:
+    index, mode = FORMATS[fmt]
+    width, height = im.size
+    return encode(im.convert(mode).tobytes(), widht, height, index)
--- test.py
+++ test.py
@@ -15,31 +15,30 @@ def get_same_ratio(buf1, buf2):
     return s / len(buf1)
 
 
-def test(image_name, fmt, mode=None):
+def test(image_name, fmt):
+    index, mode = FORMATS[fmt]
     im = Image.open(image_name)
     w, h = im.size
-    if mode is None:
-        mode = im.mode
-    else:
+    if mode != im.mode:
         im = im.convert(mode)
     raw = im.tobytes()
 
     encode_buf = get_encoded_buf(image_name, fmt)
-    decode_buf = decode(encode_buf, w, h, FORMATS[fmt])
+    decode_buf = decode(encode_buf, w, h, index)
 
     im = Image.new(mode, (w, h))
     im.frombytes(decode_buf)
-    im.save(f'{FORMATS[fmt]}_{fmt}.png')
+    im.save(f'{index}_{fmt}.png')
 
-    my_encode_buf = encode(raw, w, h, FORMATS[fmt])
+    my_encode_buf = encode(raw, w, h, index)
     ratio = get_same_ratio(my_encode_buf, encode_buf * 100)
-    print(f'*** {FORMATS[fmt]} {fmt} {ratio*100:.2f}% ***')
-    open(f'{FORMATS[fmt]}_{fmt}.bin', 'wb').write(encode_buf)
-    open(f'{FORMATS[fmt]}_{fmt}_my.bin', 'wb').write(my_encode_buf)
+    print(f'*** {index} {fmt} {ratio*100:.2f}% ***')
+    open(f'{index}_{fmt}.bin', 'wb').write(encode_buf)
+    open(f'{index}_{fmt}_my.bin', 'wb').write(my_encode_buf)
 
     im = Image.new(mode, (w, h))
-    im.frombytes(decode(my_encode_buf, w, h, FORMATS[fmt]))
-    im.save(f'{FORMATS[fmt]}_{fmt}_my.png')
+    im.frombytes(decode(my_encode_buf, w, h, index))
+    im.save(f'{index}_{fmt}_my.png')
 
 
 test('rgba.tga', 'RGBA8888')
@@ -47,10 +46,10 @@ test('rgb.tga', 'RGB888')
 test('rgba.tga', 'RGBA5551')
 test('rgb.tga', 'RGB565')
 test('rgba.tga', 'RGBA4444')
-test('rgba.tga', 'LA88', 'LA')
+test('rgba.tga', 'LA88',)
 test('rgb.tga', 'HL8')
-test('rgb.tga', 'L8', 'L')
-test('rgb.tga', 'A8', 'L')
-test('rgba.tga', 'LA44', 'LA')
+test('rgb.tga', 'L8')
+test('rgb.tga', 'A8')
+test('rgba.tga', 'LA44')
 test('rgb.tga', 'ETC1')
 test('rgba.tga', 'ETC1_A4')
